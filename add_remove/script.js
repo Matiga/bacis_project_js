9b51fd89bf21d2874e67023098903d5de2149213
Matiga <73221710+Matiga@users.noreply.github.com>
2022-05-20 14:10:53 +0800

    Add files via upload

---
+++ add_remove/script.js
@@ -0,0 +1,8 @@
+const removed = document.getElementById("heading");
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click',()=>{
+    //removed.classList.remove("remove");
+    removed.classList.add("adding");
+
+})
