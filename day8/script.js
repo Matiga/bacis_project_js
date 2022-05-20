479fc22ebc954ff194fdbfd0b1ac048f3321f88b
Matiga <73221710+Matiga@users.noreply.github.com>
2022-05-20 14:06:13 +0800

    Add files via upload

--- day8/script.js
+++ day8/script.js
@@ -1,13 +1,8 @@
-const moves = document.getElementById("move");
-const images = document.querySelectorAll("img");
+const removed = document.getElementById("heading");
+const btn = document.getElementById('btn');
 
-let start = 0;
-function move(){
-    start++
-    if(start>images.length-1){
-        start=0;
-    }
-    moves.style.transform   =`translateX(${-start*80}%)`;
-}
-setInterval(move,2000)
+btn.addEventListener('click',()=>{
+    //removed.classList.remove("remove");
+    removed.classList.add("adding");
 
+})
