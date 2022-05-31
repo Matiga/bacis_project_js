0eaf6a49e021598e6c25addd041ad95bbbb3a2a8
Matiga <73221710+Matiga@users.noreply.github.com>
2022-05-31 15:51:07 +0800

    Add files via upload

--- day13/script.js
+++ day13/script.js
@@ -1,12 +1,10 @@
-const textareaEl = document.getElementById('textarea');
-const totalCounterEl = document.getElementById('total-counter');
-const remainEl = document.getElementById('remaining-counter')
-textareaEl.addEventListener("keyup",()=>{
-    updateCounter()
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bottom-container")
+//console.log(bottomContainerEl.offsetTop)
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > bottomContainerEl.offsetTop -navbarEl.offsetHeight - 50){
+        navbarEl.classList.add("active")
+    }else{
+        navbarEl.classList.remove("active");
+    }
 })
-updateCounter()//display total = 0 and remaining=50
-
-function updateCounter(){
-    totalCounterEl.innerText = textareaEl.value.length
-    remainEl.innerText = textareaEl.getAttribute("maxLength") - textareaEl.value.length;
-}
